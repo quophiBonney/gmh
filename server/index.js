b74cb1e6539e12bb041bd5c9ccaae4853854a846
quophiBonney <quophiclef@gmail.com>
2025-09-18 15:32:22 +0000

    fifth commit

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-const connectDB = require("./config/dbconnection");
+const connectDB = require("./config/dbconnection.js");
 const getSitesRouter = require("./routes/sites_route.js");
 
 // Connect to MongoDB
